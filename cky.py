645dff3fd3b9a59848024b5e4b43f6ec32fcaede
emmabateman <emmalbateman@gmail.com>
2018-12-29 21:46:25 -0800

    CKY algorithm with CNF preprocessing

---
+++ cky.py
@@ -0,0 +1,104 @@
+import sys
+import string
+
+import nltk
+from nltk import grammar
+from nltk import Tree
+import nltk.tokenize
+
+import cnf
+
+global cnf_grammar
+global sentences
+
+#Check whether two trees represent the same parse.
+def check_duplicate(tree1, tree2):
+    match = True
+    if len(tree1) == len(tree2):
+        for i in range(len(tree1)):
+            if tree1[i] in tree1.leaves() and tree2[i] in tree2.leaves():
+                if not tree1[i] == tree2[i]:
+                    match = False
+            elif not check_duplicate(tree1[i], tree2[i]):
+                match = False
+    else:
+        return False
+    return match
+
+#Read grammar and sentences to parse from files.
+def init():
+    global cnf_grammar, sentences
+    cnf_grammar = cnf.convert(sys.argv[1])
+    f = open(sys.argv[2])
+    sentences = f.readlines()
+
+#Parse using cky algorithm.
+def parse(words):
+    n = len(words)
+    #create table
+    table = []
+    for i in range(n+1):
+        table.append([])
+        for j in range(n+1):
+            table[i].append([])
+
+    for j in range(1, n+1):
+        table[j-1][j] += findtrees_from_terminal(words[j-1])
+        r = range(0, n-1)
+        r.reverse()
+        for i in r:
+            for k in range(i+1, j):
+                for tree1 in table[i][k]:
+                    for tree2 in table[k][j]:
+                        parselist = findparse_from_trees(tree1, tree2)
+                        for parse in parselist:
+                            table[i][j].append(parse)
+
+    return table
+
+#Find nonterminal(s) for 2 nonterminals.
+def findparse_from_trees(tree1, tree2):
+    ret_list = []
+    for production in cnf_grammar.productions():
+        if (production.rhs()[0] == tree1.label()
+            and production.rhs()[-1] == tree2.label()):
+            ret_list.append(Tree(production.lhs(), [tree1, tree2]))
+    return ret_list
+
+#Find nonterminal(s) for terminal.
+def findtrees_from_terminal(word):
+    ret_list = []
+    for production in cnf_grammar.productions():
+        if word in production.rhs():
+            ret_list.append(Tree(production.lhs(), [word]))
+    return ret_list
+
+#Print the list of parses.
+def printfinal(table):
+    parse_list = table[0][-1]
+
+    #remove duplicates
+    for i in range(len(parse_list)-1):
+        for j in parse_list[i+1:]:
+            if check_duplicate(parse_list[i], j):
+                parse_list.remove(j)
+
+    for parse in table[0][-1]:
+        print(parse)
+    print("Number of parses: " + str(len(table[0][-1])))
+    return len(table[0][-1])
+
+def main():
+    global cnf_grammar, sentences
+    init()
+    for sentence in sentences:
+        #tokenize
+        words = nltk.word_tokenize(sentence)
+        #parse
+        table = parse(words)
+        print(sentence[:-1])
+        printfinal(table)
+        if sentences.index(sentence) != len(sentences)-1:
+            print("\n")
+
+main()
